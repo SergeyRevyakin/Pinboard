e2d6456acdb19aed55ad310211a9309d22042bf2
SergeyRevyakin <SergeyRevyakin@users.noreply.github.com>
2020-02-17 00:45:45 +0300

    LoginAdded

--- app/src/main/java/com/pinboard/MainActivity.kt
+++ app/src/main/java/com/pinboard/MainActivity.kt
@@ -1,34 +1,51 @@
 package com.pinboard
 
+
+
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.text.TextUtils
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
 
-    fun onClickButton(view: View){
+
+class MainActivity : AppCompatActivity(){
+
+
+    private var mAuth: FirebaseAuth? = null
+    private var mDatabase: DatabaseReference? = null
+    private var mMessageReference: DatabaseReference? = null
+
+
+    fun firebaseConnect() {
+        mDatabase = FirebaseDatabase.getInstance().reference
+        mMessageReference = FirebaseDatabase.getInstance().getReference("message")
+    }
+
+
+    fun onClickButton(view: View) {
         val database = FirebaseDatabase.getInstance()
         val myRef = database.getReference("message")
 
         myRef.setValue("Hello, Serg!")
-        val myToast = Toast.makeText(this,"DONE!",Toast.LENGTH_LONG)
+        val myToast = Toast.makeText(this, "DONE!", Toast.LENGTH_LONG)
         myToast.show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
@@ -40,19 +57,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //menuInflater.inflate(R.menu.menu_main, menu)
+        //Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
 
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+//         Handle action bar item clicks here. The action bar will
+//         automatically handle clicks on the Home/Up button, so long
+//         as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
 }
+
