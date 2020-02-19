7a9fa70293d066bcda034a54732d5581e3808a24
SergeyRevyakin <SergeyRevyakin@users.noreply.github.com>
2020-02-20 02:58:26 +0300

    startingInterface

--- app/src/main/java/com/pinboard/LoginActivity.kt
+++ app/src/main/java/com/pinboard/LoginActivity.kt
@@ -13,13 +13,14 @@ import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.login_activity.*
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
     private val TAG = "LoginActivity"
+
     private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
-
 
         btn_email_sign_in.setOnClickListener(this)
         btn_email_create_account.setOnClickListener(this)
@@ -37,7 +38,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View?) {
-        val i = view!!.id
+        val i = (view ?: return).id
 
         when (i) {
             R.id.btn_email_create_account -> createAccount(
@@ -120,8 +121,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
         if (password.length < 6) {
             Toast.makeText(
-                applicationContext,
-                "Password too short, enter minimum 6 characters!",
+                applicationContext, "Password too short, enter minimum 6 characters!",
                 Toast.LENGTH_SHORT
             ).show()
             return false
@@ -167,4 +167,161 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         startActivity(Intent(this, MessageActivity::class.java))
     }
 }
+
+
+//class LoginActivity : AppCompatActivity(), View.OnClickListener {
+//    private val TAG = "LoginActivity"
+//    private var mAuth: FirebaseAuth? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.login_activity)
+//
+//
+//        btn_email_sign_in.setOnClickListener(this)
+//        btn_email_create_account.setOnClickListener(this)
+//        btn_sign_out.setOnClickListener(this)
+//        btn_test_message.setOnClickListener(this)
+//
+//        mAuth = FirebaseAuth.getInstance()
+//    }
+//
+//    override fun onStart() {
+//        super.onStart()
+//
+//        val currentUser = mAuth!!.currentUser
+//        updateUI(currentUser)
+//    }
+//
+//    override fun onClick(view: View?) {
+//        val i = view!!.id
+//
+//        when (i) {
+//            R.id.btn_email_create_account -> createAccount(
+//                edtEmail.text.toString(),
+//                edtPassword.text.toString()
+//            )
+//            R.id.btn_email_sign_in -> signIn(edtEmail.text.toString(), edtPassword.text.toString())
+//            R.id.btn_sign_out -> signOut()
+//            R.id.btn_test_message -> testMessage()
+//        }
+//    }
+//
+//    private fun createAccount(email: String, password: String) {
+//        Log.e(TAG, "createAccount:" + email)
+//        if (!validateForm(email, password)) {
+//            return
+//        }
+//
+//        mAuth!!.createUserWithEmailAndPassword(email, password)
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    Log.e(TAG, "createAccount: Success!")
+//
+//                    // update UI with the signed-in user's information
+//                    val user = mAuth!!.currentUser
+//                    updateUI(user)
+//                    writeNewUser(user!!.uid, getUsernameFromEmail(user.email), user.email)
+//                } else {
+//                    Log.e(TAG, "createAccount: Fail!", task.exception)
+//                    Toast.makeText(applicationContext, "Authentication failed!", Toast.LENGTH_SHORT)
+//                        .show()
+//                    updateUI(null)
+//                }
+//            }
+//    }
+//
+//    private fun signIn(email: String, password: String) {
+//        Log.e(TAG, "signIn:" + email)
+//        if (!validateForm(email, password)) {
+//            return
+//        }
+//
+//        mAuth!!.signInWithEmailAndPassword(email, password)
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    Log.e(TAG, "signIn: Success!")
+//
+//                    // update UI with the signed-in user's information
+//                    val user = mAuth!!.currentUser
+//                    updateUI(user)
+//                } else {
+//                    Log.e(TAG, "signIn: Fail!", task.exception)
+//                    Toast.makeText(applicationContext, "Authentication failed!", Toast.LENGTH_SHORT)
+//                        .show()
+//                    updateUI(null)
+//                }
+//
+//                if (!task.isSuccessful) {
+//                    tvStatus.text = "Authentication failed!"
+//                }
+//            }
+//    }
+//
+//    private fun signOut() {
+//        mAuth!!.signOut()
+//        updateUI(null)
+//    }
+//
+//    private fun validateForm(email: String, password: String): Boolean {
+//
+//        if (TextUtils.isEmpty(email)) {
+//            Toast.makeText(applicationContext, "Enter email address!", Toast.LENGTH_SHORT).show()
+//            return false
+//        }
+//
+//        if (TextUtils.isEmpty(password)) {
+//            Toast.makeText(applicationContext, "Enter password!", Toast.LENGTH_SHORT).show()
+//            return false
+//        }
+//
+//        if (password.length < 6) {
+//            Toast.makeText(
+//                applicationContext,
+//                "Password too short, enter minimum 6 characters!",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//            return false
+//        }
+//
+//        return true
+//    }
+//
+//    private fun updateUI(user: FirebaseUser?) {
+//
+//        if (user != null) {
+//            tvStatus.text = "User Email: " + user.email
+//            tvDetail.text = "Firebase User ID: " + user.uid
+//
+//            email_password_buttons.visibility = View.GONE
+//            email_password_fields.visibility = View.GONE
+//            layout_signed_in_buttons.visibility = View.VISIBLE
+//        } else {
+//            tvStatus.text = "Signed Out"
+//            tvDetail.text = null
+//
+//            email_password_buttons.visibility = View.VISIBLE
+//            email_password_fields.visibility = View.VISIBLE
+//            layout_signed_in_buttons.visibility = View.GONE
+//        }
+//    }
+//
+//    private fun writeNewUser(userId: String, username: String?, email: String?) {
+//        val user = User(username, email)
+//
+//        FirebaseDatabase.getInstance().reference.child("users").child(userId).setValue(user)
+//    }
+//
+//    private fun getUsernameFromEmail(email: String?): String {
+//        return if (email!!.contains("@")) {
+//            email.split("@".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
+//        } else {
+//            email
+//        }
+//    }
+//
+//    private fun testMessage() {
+//        startActivity(Intent(this, MessageActivity::class.java))
+//    }
+//}
 
--- app/src/main/java/com/pinboard/MessageActivity.kt
+++ app/src/main/java/com/pinboard/MessageActivity.kt
@@ -141,7 +141,7 @@ class MessageActivity : AppCompatActivity() {
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                Log.e(TAG, "postMessages:onCancelled", databaseError!!.toException())
+                Log.e(TAG, "postMessages:onCancelled", databaseError.toException())
                 Toast.makeText(this@MessageActivity, "Failed to load Pin.", Toast.LENGTH_SHORT)
                     .show()
             }
@@ -179,21 +179,21 @@ class MessageActivity : AppCompatActivity() {
         // User data change listener
         mDatabase!!.child("users").child(user!!.uid)
             .addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val user = dataSnapshot.getValue(User::class.java)
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val user = dataSnapshot.getValue(User::class.java)
 
-                if (user == null) {
-                    Log.e(TAG, "onDataChange: User data is null!")
-                    Toast.makeText(
-                        this@MessageActivity,
-                        "onDataChange: User data is null!",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return
+                    if (user == null) {
+                        Log.e(TAG, "onDataChange: User data is null!")
+                        Toast.makeText(
+                            this@MessageActivity,
+                            "onDataChange: User data is null!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return
+                    }
+
+                    writeNewMessage(body)
                 }
-
-                writeNewMessage(body)
-            }
 
                 override fun onCancelled(error: DatabaseError) {
                     // Failed to read value
@@ -226,136 +226,3 @@ class MessageActivity : AppCompatActivity() {
     }
 }
 
-
-//    private val TAG = "MessageActivity"
-//    private val REQUIRED = "Required"
-//
-//    private var user: FirebaseUser? = null
-//
-//    private var mDatabase: DatabaseReference? = null
-//    private var mMessageReference: DatabaseReference? = null
-//    private var mMessageListener: ValueEventListener? = null
-//
-//    var messageNum = 0
-//
-//
-//    private fun genereteValues(): List<String> {
-//        val values = mutableListOf<String>()
-//        for (i in 0..100) {
-//            values.add("$i element")
-//        }
-//        return values
-//    }
-//
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.message_activity)
-//
-//        mDatabase = FirebaseDatabase.getInstance().reference
-//        mMessageReference = FirebaseDatabase.getInstance().getReference("message")
-//        user = FirebaseAuth.getInstance().currentUser
-//
-//        btnSend.setOnClickListener {
-//            submitMessage()
-//            edtSentText.setText("")
-//        }
-//
-//        btnBack.setOnClickListener {
-//            finish()
-//        }
-//
-//        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
-//        recyclerView.layoutManager = LinearLayoutManager(this)
-//        recyclerView.adapter = Classified(genereteValues())
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-//
-//        val messageListener = object : ValueEventListener {
-//            override fun onDataChange(dataSnapshot: DataSnapshot) {
-//                if (dataSnapshot.exists()) {
-////                    val message = dataSnapshot.getValue(Pin::class.java)
-////
-////                    Log.e(TAG, "onDataChange: Pin data is updated: " + message!!.author + ", " + message.time + ", " + message.body)
-////
-////                    tvAuthor.text = message.author
-////                    tvTime.text = message.time
-////                    tvBody.text = message.body
-//
-//                }
-//            }
-//
-//            override fun onCancelled(databaseError: DatabaseError) {
-//                // Failed to read value
-//                Log.e(TAG, "onCancelled: Failed to read message")
-//
-//                tvAuthor.text = ""
-//                tvTime.text = ""
-//                tvBody.text = "onCancelled: Failed to read message!"
-//            }
-//        }
-//
-//        mMessageReference!!.addValueEventListener(messageListener)
-//
-//        // copy for removing at onStop()
-//        mMessageListener = messageListener
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//
-//        if (mMessageListener != null) {
-//            mMessageReference!!.removeEventListener(mMessageListener!!)
-//        }
-//    }
-//
-//    private fun submitMessage() {
-//        val body = edtSentText.text.toString()
-//
-//        if (TextUtils.isEmpty(body)) {
-//            edtSentText.error = REQUIRED
-//            return
-//        }
-//
-//        // User data change listener
-////        mDatabase!!.child("users").child(user!!.uid).addListenerForSingleValueEvent(object : ValueEventListener {
-////            override fun onDataChange(dataSnapshot: DataSnapshot) {
-////                val user = dataSnapshot.getValue(User::class.java)
-////
-////                if (user == null) {
-////                    Log.e(TAG, "onDataChange: User data is null!")
-////                    Toast.makeText(this@MessageActivity, "onDataChange: User data is null!", Toast.LENGTH_SHORT).show()
-////                    return
-////                }
-////
-////                writeNewMessage(body)
-////            }
-////
-////            override fun onCancelled(error: DatabaseError) {
-////                // Failed to read value
-////                Log.e(TAG, "onCancelled: Failed to read user!")
-////            }
-////        })
-//        writeNewMessage(body)
-//    }
-//
-//    private fun writeNewMessage(body: String) {
-//        val time = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().time)
-//        val message = Pin(getUsernameFromEmail(user!!.email), body, time, "Something Something Something", "111")
-//        messageNum++
-//        //mMessageReference!!.setValue(message)
-//
-//        FirebaseDatabase.getInstance().reference.child("message").child(messageNum.toString())
-//            .setValue(message)
-//    }
-//
-//    private fun getUsernameFromEmail(email: String?): String {
-//        return if (email!!.contains("@")) {
-//            email.split("@".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
-//        } else {
-//            email
-//        }
-//    }
-//}
